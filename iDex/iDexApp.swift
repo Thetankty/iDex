ba6c3d71ae89d164b6f241601046044436143ded
Tyler <tyholgate16@gmail.com>
2024-02-24 17:16:25 +0000

    Initial Commit

---
+++ iDex/iDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDexApp.swift
+//  iDex
+//
+//  Created by Tyler on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
