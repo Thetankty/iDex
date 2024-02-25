0aec37c00a224d34ac61b7c56d86f6a21858065b
Tyler <tyholgate16@gmail.com>
2024-02-25 23:14:29 +0000

    Updated the ScreenView and worked on performance issues

--- iDex/PokemonAPI/PokemonApi.swift
+++ iDex/PokemonAPI/PokemonApi.swift
@@ -8,6 +8,7 @@
 import Foundation
 import PokemonAPI
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct Pokemon: Identifiable {
     let id: Int
@@ -27,23 +28,128 @@ struct SearchBar: View {
     var body: some View {
         HStack {
             TextField(placeholder, text: $text)
-                .padding(.horizontal)
-                .padding(.vertical, 8)
-                .background(Color.gray.opacity(0.3))
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
                 .cornerRadius(8)
-                .padding(.horizontal)
-            if !text.isEmpty {
-                Button(action: {
-                    self.text = ""
-                }) {
-                    Image(systemName: "x.circle.fill")
-                        .foregroundColor(.gray)
-                }
-                .padding(.trailing, 8)
-            }
+                .padding(.horizontal, 10)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .padding(.leading, 15) // Add padding to the leading edge of the magnifying glass icon
+                            .padding(.trailing, 4) // Add padding to the trailing edge of the magnifying glass icon
+
+                        Spacer()
+                        
+                        if !text.isEmpty {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
         }
+        .padding(.top, 10)
+        .padding(.bottom, 10)
     }
 }
 
 
 
+
+struct PokemonMainScreen: View {
+    let pokemon: Pokemon
+    @State private var selectedTab = 0
+
+    var body: some View {
+        VStack {
+            if let spriteURL = pokemon.spriteURL {
+                WebImage(url: spriteURL)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+            } else {
+                Text("Sprite not available")
+            }
+            Text("Name: \(capitalizeFirstLetter(pokemon.name))")
+            Text("Pokedex Number: \(pokemon.id)")
+            Text("Height: \(pokemon.height)")
+            Text("Weight: \(pokemon.weight)")
+
+            HStack {
+                Spacer()
+                Button(action: {
+                    selectedTab = 0
+                }) {
+                    Text("Abilities")
+                        .padding()
+                        .background(selectedTab == 0 ? Color.blue : Color.clear)
+                        .foregroundColor(selectedTab == 0 ? .white : .blue)
+                        .cornerRadius(10)
+                }
+                Spacer()
+                Button(action: {
+                    selectedTab = 1
+                }) {
+                    Text("Moves")
+                        .padding()
+                        .background(selectedTab == 1 ? Color.blue : Color.clear)
+                        .foregroundColor(selectedTab == 1 ? .white : .blue)
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
+            .padding()
+
+            if selectedTab == 0 {
+                VStack {
+                    Text("Abilities:")
+                    ForEach(pokemon.abilities, id: \.self) { ability in
+                        Text(capitalizeFirstLetter(ability))
+                    }
+                }
+            } else {
+                ScrollView{
+                    VStack {
+                        Text("Moves:")
+                        ForEach(pokemon.moves, id: \.self) { move in
+                            Text(capitalizeFirstLetter(move))
+                        }
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+struct PokemonItemView: View {
+    let pokemon: Pokemon
+    
+    var body: some View {
+        VStack {
+            if let spriteURL = pokemon.spriteURL {
+                WebImage(url: spriteURL)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                    .padding(.bottom, 5)
+            } else {
+                Text("Sprite not available")
+            }
+            Text(pokemon.name.capitalized) // Display Pokémon's name
+                .foregroundColor(.white)
+                .padding(.top, 5)
+        }
+        .padding(10) // Add padding around the whole VStack
+        .foregroundColor(Color(.white))
+        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.971, opacity: 0.25).blur(radius: 10))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
--- iDex/PokemonScreenView.swift
+++ iDex/PokemonScreenView.swift
@@ -9,24 +9,26 @@ import Foundation
 import PokemonAPI
 import SwiftUI
 import FluidGradient
-
+import SDWebImageSwiftUI
 
 struct PokemonScreen: View {
     @State private var selectedPokemon: Pokemon?
     @State private var pokemonList: [Pokemon] = []
     @State private var isSearchBarVisible = false
     @State private var searchText = ""
-
+    private let batchSize = 50 // Adjust batch size as needed
+    
     var body: some View {
         NavigationView {
-            ZStack{
+            ZStack {
                 FluidGradient(blobs: [.red, .green, .blue],
                               highlights: [.yellow, .orange, .purple],
-                              speed: 1.0,
+                              speed: 0.4,
                               blur: 0.75)
                     .edgesIgnoringSafeArea(.all)
-                VStack {
-                    
+                    .preferredColorScheme(.dark)
+                
+                VStack(spacing: 20) {
                     if isSearchBarVisible || searchText.isEmpty {
                         ScrollView {
                             LazyVGrid(columns: Array(repeating: GridItem(), count: 3)) {
@@ -36,28 +38,36 @@ struct PokemonScreen: View {
                                     }
                                 }
                             }
+                            .padding(.bottom, 20) // Add padding to move the grid items down 20px
                         }
                     }
                 }
-                .navigationTitle(Text("Pokémon").font(.headline))
-                .navigationBarItems(trailing:
-                                        Group {
-                    if isSearchBarVisible {
-                        SearchBar(text: $searchText, placeholder: "Search Pokémon")
-                            .padding()
-                    } else {
-                        Button(action: {
-                            isSearchBarVisible.toggle()
-                        }) {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(.primary)
-                                .font(.title)
-                        }
-                        .padding()
-                    }
-                }
-                )
+                .padding(.top, 20) // Add padding to move the title and search bar down 20px
             }
+            .navigationBarItems(
+                leading:
+                    Text("Pokémon")
+                        .font(.largeTitle)
+                        .padding(.top, 20), // Add padding to move the title down 20px
+                trailing:
+                    HStack {
+                        if isSearchBarVisible {
+                            SearchBar(text: $searchText, placeholder: "Search Pokémon")
+                                .padding(.top, 30)
+                        } else {
+                            Button(action: {
+                                isSearchBarVisible.toggle()
+                            }) {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.primary)
+                                    .font(.title)
+                                    .padding(.trailing, 8)
+                                    .padding(.top, 30)
+                            }
+                        }
+                    }
+                    .padding(.bottom) // Add more margin under the navigation bar items
+            )
             .onAppear {
                 fetchPokemonList()
             }
@@ -72,134 +82,38 @@ struct PokemonScreen: View {
         }
     }
 
-
     func fetchPokemonList() {
-        let numberOfPokemon = 898 // Total number of Pokémon
-        let pokemonIDs = Array(1...numberOfPokemon)
-        pokemonIDs.forEach { id in
-            PokemonAPI().pokemonService.fetchPokemon(id) { result in
-                switch result {
-                case .success(let pkmpokemon):
-                    if var name = pkmpokemon.name {
-                        // Capitalize the first letter only
-                        name = name.prefix(1).capitalized + name.dropFirst()
-                        
-                        // Fetch sprite URL
-                        if let sprites = pkmpokemon.sprites {
-                            if let frontDefault = sprites.frontDefault {
-                                let spriteURL = URL(string: frontDefault)
-                                let height = pkmpokemon.height ?? 0 // Set default height if nil
-                                let weight = pkmpokemon.weight ?? 0 // Set default weight if nil
-                                let abilities = pkmpokemon.abilities?.compactMap { $0.ability?.name } ?? [] // Fetch abilities
-                                let types = pkmpokemon.types?.compactMap { $0.type?.name } ?? [] // Fetch types
-                                
-                                let moves = pkmpokemon.moves?.compactMap { $0.move?.name } ?? []
-                                
-                                let pokemon = Pokemon(id: id, name: name, height: height, weight: weight, abilities: abilities, spriteURL: spriteURL, types: types, moves: moves)
+        DispatchQueue.global().async {
+            for id in 1...898 { // Fetch all Pokémon IDs
+                PokemonAPI().pokemonService.fetchPokemon(id) { result in
+                    switch result {
+                    case .success(let pkmpokemon):
+                        if let name = pkmpokemon.name,
+                           let sprites = pkmpokemon.sprites,
+                           let frontDefault = sprites.frontDefault,
+                           let spriteURL = URL(string: frontDefault),
+                           let height = pkmpokemon.height,
+                           let weight = pkmpokemon.weight,
+                           let abilities = pkmpokemon.abilities?.compactMap({ $0.ability?.name }),
+                           let types = pkmpokemon.types?.compactMap({ $0.type?.name }),
+                           let moves = pkmpokemon.moves?.compactMap({ $0.move?.name }) {
+                            
+                            let pokemon = Pokemon(id: id, name: name, height: height, weight: weight, abilities: abilities, spriteURL: spriteURL, types: types, moves: moves)
+                            
+                            DispatchQueue.main.async {
                                 pokemonList.append(pokemon)
-                                pokemonList.sort { $0.id < $1.id } // Sort the pokemonList by ID
+                                pokemonList.sort { $0.id < $1.id } // Sort the list
                             }
                         }
+                    case .failure(let error):
+                        print("Error fetching Pokémon with ID \(id): \(error)")
                     }
-                case .failure(let error):
-                    print("Error fetching Pokémon with ID \(id): \(error)")
                 }
             }
         }
     }
+
 }
-
-struct PokemonItemView: View {
-    let pokemon: Pokemon
-
-    var body: some View {
-        VStack {
-            if let spriteURL = pokemon.spriteURL {
-                AsyncImage(url: spriteURL)
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
-            } else {
-                Text("Sprite not available")
-            }
-            Text(pokemon.name) // Display Pokémon's name
-                .padding(.top, 5) // Add padding at the top of the text
-        }
-        .padding(10) // Add padding around the whole VStack
-        .background(Color.gray.opacity(0.3))
-        .cornerRadius(10)
-        .frame(width: 120, height: 150) // Set fixed size for the frame
-    }
-}
-
-struct PokemonMainScreen: View {
-    let pokemon: Pokemon
-    @State private var selectedTab = 0
-
-    var body: some View {
-        VStack {
-            if let spriteURL = pokemon.spriteURL {
-                AsyncImage(url: spriteURL)
-                    .frame(width: 100, height: 100)
-            } else {
-                Text("Sprite not available")
-            }
-            Text("Name: \(capitalizeFirstLetter(pokemon.name))")
-            Text("Pokedex Number: \(pokemon.id)")
-            Text("Height: \(pokemon.height)")
-            Text("Weight: \(pokemon.weight)")
-
-            // Add navigation bar with tabs for abilities and moves
-            HStack {
-                Spacer()
-                Button(action: {
-                    selectedTab = 0
-                }) {
-                    Text("Abilities")
-                        .padding()
-                        .background(selectedTab == 0 ? Color.blue : Color.clear)
-                        .foregroundColor(selectedTab == 0 ? .white : .blue)
-                        .cornerRadius(10)
-                }
-                Spacer()
-                Button(action: {
-                    selectedTab = 1
-                }) {
-                    Text("Moves")
-                        .padding()
-                        .background(selectedTab == 1 ? Color.blue : Color.clear)
-                        .foregroundColor(selectedTab == 1 ? .white : .blue)
-                        .cornerRadius(10)
-                }
-                Spacer()
-            }
-            .padding()
-
-            // Display content based on the selected tab
-            if selectedTab == 0 {
-                // Display abilities
-                VStack {
-                    Text("Abilities:")
-                    ForEach(pokemon.abilities, id: \.self) { ability in
-                        Text(capitalizeFirstLetter(ability))
-                    }
-                }
-            } else {
-                // Display moves
-                ScrollView{
-                    VStack {
-                        Text("Moves:")
-                        ForEach(pokemon.moves, id: \.self) { move in
-                            Text(capitalizeFirstLetter(move))
-                        }
-                    }
-                }
-            }
-            Spacer()
-        }
-    }
-}
-
-
 
 
 struct PokeScreen_Previews: PreviewProvider {
@@ -207,4 +121,3 @@ struct PokeScreen_Previews: PreviewProvider {
         PokemonScreen()
     }
 }
-
